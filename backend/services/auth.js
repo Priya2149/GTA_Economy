c78f89e92c135a2cd07e403461350e03b4a7b2ae
Priya Prajapati <priya2572001@gmail.com>
2025-04-08 14:09:45 -0400

    Revert "Initial commit"
    This reverts commit 969db08b1fdb186582a3953053099d76fb111957.

--- backend/services/auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const login = async (email, password) => {
-    const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
-    localStorage.setItem('token', response.data.token);  // Store JWT token
-    return response.data;
-};
-
-export { login };
